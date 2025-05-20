2a6c173fc5d4604923b448c724375879a77f11ab
LearFum <Learfum2000@yandex.ru>
2025-05-20 21:14:51 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+// Можно добавить анимации или интерактив
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Спасибо за сообщение! Шрек получит его, когда выйдет из болота.');
+});
